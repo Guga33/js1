bbfb444cb7f2373d614bd389760a7a0fcc904fa7
Guga <gaprindashviliguga@gmail.com>
2020-09-23 14:00:05 +0400

    s

---
+++ week1/funct.js
@@ -0,0 +1,11 @@
+function printHello(){
+    document.write("Hello <hr>");
+ }
+ 
+ function hello(name){
+    document.write("Hello "+name+"<hr>");
+ }
+ 
+ function italicText(text){
+    document.write("<h2 class='italic'>"+text+"</h2>");
+ }
